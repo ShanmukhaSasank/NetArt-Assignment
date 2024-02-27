89349ca87441d7be9e56dabe2def34bb4a7e06bd
shanu1981 <shanu.90158@gmail.com>
2024-02-27 17:58:07 +0530

    Few fixes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import Header from './components/header/header';
 import Navbar from './components/navbar/navbar';
 import Information from './components/information/information';
 import Footer from './components/footer/footer';
+import styled from '@emotion/styled';
+
+const HeaderContainer = styled(Box)`
+    background: linear-gradient(to bottom, #fffff5, #fff9bb); /* Gradient colors */
+    padding: 20px; /* Add padding for spacing */
+`;
 
 function App() {
   return (
     <>
+    <HeaderContainer>
       <Header/>
       <Box sx={{ maxWidth: '100%', overflowX: 'hidden' }}>
         <Navbar></Navbar>
       </Box>
       <Information/>
       <Footer></Footer>
+    </HeaderContainer>
     </>
   );
 }
